489d6da513f4636047368dc52ee156a869a09d75
Hernan Pedrino <hernanpedrino@gmail.com>
2021-12-22 11:38:43 -0300

    Card y Navbar listos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { CardApp } from './components/card';
 import { Navbarapp } from './components/navbar';
 
 function App() {
   return (
     <div>
       <Navbarapp/>
+      <CardApp/>
       <h1>Tienda next U</h1>
     </div>
   );
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 export const Navbarapp = () => {
     return (
         
-        <Navbar bg="dark">
+        <Navbar bg="dark" variant='dark'>
             <Container>
                 <Navbar.Brand href="#home">Navbar</Navbar.Brand>
                 <Nav className="me-auto">
@@ -12,6 +12,9 @@ export const Navbarapp = () => {
                 <Nav.Link href="#features">Features</Nav.Link>
                 <Nav.Link href="#pricing">Pricing</Nav.Link>
                 </Nav>
+                <span style={{color: 'White'}}>
+                    <i className='fas fa-shopping-cart fa-2x'></i>
+                </span>
             </Container>
         </Navbar>
        
